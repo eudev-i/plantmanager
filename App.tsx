2f80edb010578e278cf782063c6cc2dbd54fef4b
Vitoria Gonçalves <vitoria.goncalvess.gaspar@gmail.com>
2021-04-22 08:57:14 -0300

    tela de identificação e confirmação de usuários

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Welcome } from './src/pages/Welcome';
 import AppLoading from "expo-app-loading";
 import {useFonts, Jost_400Regular, Jost_600SemiBold} from '@expo-google-fonts/jost'
+import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
 
 export default function App(){
   const [ fontsLoaded ] = useFonts({
@@ -13,6 +15,6 @@ export default function App(){
     return <AppLoading/>
 
   return(
-    <Welcome/>
+    <Confirmation/>
   )
 }
